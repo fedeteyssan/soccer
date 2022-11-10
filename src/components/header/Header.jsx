c0c77d3cb9519fae2b92d255524d9ce7595fc409
federico.teyssandier <fedeteyssan@gmail.com>
2022-11-10 09:58:19 -0300

    Links home funcionando

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,7 @@
 import "./Header.scss";
-import { useRef } from "react";
+
 
 const Header = () => {
-
-    const scrollSelecciones = useRef();
-
-    const controlScroll1 = () => {
-    scrollSelecciones.current?.scrollIntoView({behavior: 'smooth'});
-    };
-
 
 
     return (
@@ -30,15 +23,6 @@ const Header = () => {
                        Elegí un camino para empezar tu búsqueda:
                     </p>
                 </div>
-
-                <div className="contenedor-imagenes">
-                    <img src= {require ("../../Multimedia/inicio-selecciones.png")} alt="" onClick={controlScroll1}/>
-                    <img src= {require ("../../Multimedia/inicio-estilos.png")} alt=""/>
-                    <img src= {require ("../../Multimedia/inicio-posiciones.png")} alt="" />
-                    <img src= {require ("../../Multimedia/inicio-jugador.png")} alt="" />
-                </div>
-
-                <p ref={scrollSelecciones} className="ScrollToSelecciones"></p>
 
             </div>
             
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,10 +6,36 @@ import Posiciones from "../posiciones/Posiciones";
 import Estilos from "../estilos/Estilos";
 import { Container } from "react-bootstrap";
 import ListadoBotinesJugador from "../../containers/ListadoBotinesJugador";
+import { useRef } from "react";
 
 
 
 const Home = (props) => {
+
+    const scrollSelecciones = useRef();
+
+    const controlScrollSelecciones = () => {
+    scrollSelecciones.current?.scrollIntoView({behavior: 'smooth'});
+    };
+
+    const scrollEstilos = useRef();
+
+    const controlScrollEstilos = () => {
+    scrollEstilos.current?.scrollIntoView({behavior: 'smooth'});
+    };
+
+    const scrollPosiciones = useRef();
+
+    const controlScrollPosiciones = () => {
+    scrollPosiciones.current?.scrollIntoView({behavior: 'smooth'});
+    };
+
+    const scrollJugador = useRef();
+
+    const controlScrollJugador = () => {
+    scrollJugador.current?.scrollIntoView({behavior: 'smooth'});
+    };
+
 
     
     
@@ -17,10 +43,21 @@ const Home = (props) => {
     return (
         <Container fluid className={props.className}>
 
-            <Header></Header>
-            <TituloSeccion text="Iniciar juego por selección" className="contenedor-titulo contenedor-titulo-selecciones"/>
-            <section  className="seccion-busqueda-selecciones" id="seleccion">
+            <header>
+                <Header></Header>
 
+                <div className="contenedor-imagenes">
+                    <img src= {require ("../../Multimedia/inicio-selecciones.png")} alt="" onClick={controlScrollSelecciones}/>
+                    <img src= {require ("../../Multimedia/inicio-estilos.png")} alt="" onClick={controlScrollEstilos}/>
+                    <img src= {require ("../../Multimedia/inicio-posiciones.png")} alt="" onClick={controlScrollPosiciones}/>
+                    <img src= {require ("../../Multimedia/inicio-jugador.png")} alt="" onClick={controlScrollJugador}/>
+                </div>
+
+            </header>
+            
+            
+            <section className="seccion-busqueda-selecciones" ref={scrollSelecciones}>
+                <TituloSeccion text="Iniciar juego por selección" className="contenedor-titulo contenedor-titulo-selecciones"/>
 
 
                 <h3>Elegí una selección nacional</h3>
@@ -33,8 +70,8 @@ const Home = (props) => {
                 
             </section>
            
-            <TituloSeccion text="Iniciar juego por estilos" className="contenedor-titulo contenedor-titulo-estilos"/>
-            <section className="seccion-busqueda-estilos">
+            <section className="seccion-busqueda-estilos" ref={scrollEstilos}>
+                <TituloSeccion text="Iniciar juego por estilos" className="contenedor-titulo contenedor-titulo-estilos"/>
 
                 <h3 className="subtitulo-estilos">Elegí tu estilo para afrontar un partido</h3>
 
@@ -42,8 +79,8 @@ const Home = (props) => {
 
             </section>
       
-            <TituloSeccion text="Iniciar juego por posición" className="contenedor-titulo contenedor-titulo-posiciones"/>
-            <section className="seccion-busqueda-posiciones">
+            <section className="seccion-busqueda-posiciones"ref={scrollPosiciones} >
+                <TituloSeccion text="Iniciar juego por posición" className="contenedor-titulo contenedor-titulo-posiciones"/>
 
                 <h3>Elegí tu posición en la cancha</h3>
 
@@ -51,8 +88,9 @@ const Home = (props) => {
 
             </section>
       
-            <TituloSeccion text="Iniciar juego por jugador" className="contenedor-titulo contenedor-titulo-jugador"/>
-            <section className="seccion-busqueda-jugador">
+            
+            <section className="seccion-busqueda-jugador" ref={scrollJugador}>
+                <TituloSeccion text="Iniciar juego por jugador" className="contenedor-titulo contenedor-titulo-jugador"/>
 
                 <ListadoBotinesJugador></ListadoBotinesJugador>
             </section>

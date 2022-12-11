2cc840fb49e9e1114937ccbacef43c382fdb454a
federico.teyssandier <fedeteyssan@gmail.com>
2022-12-11 20:27:29 -0300

    Ajustes cover

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -14,8 +14,9 @@ const Cover = () => {
 
             <div className="cta">
                 <h1>JUGÁ <br/> MUNDIAL</h1>
-                <p>Cada figura de la Copa del Mundo ya eligió <br/> los botines que potencian su juego</p>
-                <h2>Ahora buscá vos tu par</h2>
+                <p>Divertite con nuestro buscador. <br/> Elegí la forma de juego que más te guste 
+                    <br/> y descubrí los botines de las figuras <br/>del mundial de Qatar 2022.
+                </p>
                 <Link to="/home"><button>EMPEZAR</button></Link>
             </div>
             
